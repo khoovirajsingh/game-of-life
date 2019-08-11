f45ddfbe787a8051502a6a9433af9648551373b8
khoovis <khoovirajsingh.seewoonauth@mkodo.com>
2019-08-11 19:45:39 +0100

    Finished game of life

--- src/main/kotlin/GameOfLife.kt
+++ src/main/kotlin/GameOfLife.kt
@@ -1,6 +1,79 @@
-class GameOfLife(private val world: World) {
-    fun nextGeneration(): World {
-        return world
+class GameOfLife(private val grid: Array<Array<Char>>) {
+    private var maxColumn = 0
+    private var maxRow = 0
+    private val DEAD = '.'
+    private val ALIVE = 'x'
+    private var nextGen = createDefaultGrid()
+
+    init {
+        if (grid.isNotEmpty()) {
+            maxRow = grid.size
+            maxColumn = grid[0].size
+            nextGen = createDefaultGrid()
+        }
     }
 
+    private fun createDefaultGrid() = Array(maxRow) { Array(maxColumn) { '.' } }
+
+    fun nextGeneration(): Array<Array<Char>> {
+        rows().forEach { row ->
+            columns().forEach { column ->
+                liveCellRemainsAlive(row, column)
+                deadCellRegenerates(row, column)
+            }
+        }
+        return nextGen
+    }
+
+    private fun rows(): IntRange = grid.indices
+
+    private fun columns(): IntRange = grid[0].indices
+
+    private fun liveCellRemainsAlive(row: Int, column: Int) {
+        val cell = grid[row][column]
+        if (isAlive(cell)) {
+            when (liveNeighboursAround(row, column)) {
+                2, 3 -> cellIsAlive(row, column)
+            }
+        }
+    }
+
+    private fun deadCellRegenerates(row: Int, column: Int) {
+        val cell = grid[row][column]
+        if (isDead(cell)) {
+            when (liveNeighboursAround(row, column)) {
+                3 -> cellIsAlive(row, column)
+            }
+        }
+    }
+
+    fun liveNeighboursAround(row: Int, column: Int) =
+        numOfAliveCells(row - 1, column - 1) +
+                numOfAliveCells(row - 1, column) +
+                numOfAliveCells(row - 1, column + 1) +
+                numOfAliveCells(row, column - 1) +
+                numOfAliveCells(row, column + 1) +
+                numOfAliveCells(row + 1, column - 1) +
+                numOfAliveCells(row + 1, column) +
+                numOfAliveCells(row + 1, column + 1)
+
+    private fun numOfAliveCells(row: Int, column: Int) = when {
+        isWithinGrid(row, column) && isAlive(grid[row][column]) -> 1
+        else -> 0
+    }
+
+    private fun isWithinGrid(row: Int, column: Int) = isWithinRowBoundary(row) &&
+            isWithinColumnBoundary(column)
+
+    private fun isWithinColumnBoundary(column: Int) = column in 0 until maxColumn
+
+    private fun isWithinRowBoundary(row: Int) = row in 0 until maxRow
+
+    private fun cellIsAlive(row: Int, column: Int) {
+        nextGen[row][column] = ALIVE
+    }
+
+    private fun isAlive(cell: Char) = cell == ALIVE
+
+    private fun isDead(cell: Char) = cell == DEAD
 }
--- src/test/kotlin/GameOfLife_spec.kt
+++ src/test/kotlin/GameOfLife_spec.kt
@@ -3,14 +3,115 @@ import org.junit.Test
 
 class GameOfLife_spec {
     @Test
-    fun `next generation of world is empty if world is empty`() {
-        val emptyWorld = World()
-        val gameOfLife = GameOfLife(emptyWorld)
-        gameOfLife.nextGeneration() `should be` emptyWorld
+    fun `next generation is an empty grid if grid is empty`() {
+        val grid = arrayOf<Array<Char>>()
+
+        val gameOfLife = GameOfLife(grid)
+
+        gameOfLife.nextGeneration() contentDeepEquals grid `should be` true
     }
 
     @Test
-    fun `next generation of world contains a dead cell if world started with a dead cell`() {
+    fun `a live cell should die if grid has only 1 live cell`() {
+        val grid = arrayOf(arrayOf('x'))
+        val gameOfLife = GameOfLife(grid)
 
+        val nextGenGrid = gameOfLife.nextGeneration()
+
+        grid contentDeepEquals nextGenGrid `should be` false
+    }
+
+    @Test
+    fun `a dead cell should stay if grid has only 1 dead cell`() {
+        val grid = arrayOf(arrayOf('.'))
+        val gameOfLife = GameOfLife(grid)
+
+        val nextGenGrid = gameOfLife.nextGeneration()
+
+        grid contentDeepEquals nextGenGrid `should be` true
+    }
+
+
+    @Test
+    fun `count alive neighbours in a 1x1 grid`() {
+        val grid = arrayOf(arrayOf('.'))
+        val gameOfLife = GameOfLife(grid)
+
+        val aliveNeighbours = gameOfLife.liveNeighboursAround(row = 0, column = 0)
+
+        aliveNeighbours `should be` 0
+    }
+
+    @Test
+    fun `count alive neighbours in a 2x2 grid`() {
+        val grid = arrayOf(
+            arrayOf('x', '.'),
+            arrayOf('x', '.'))
+        val gameOfLife = GameOfLife(grid)
+
+        var aliveNeighbours = gameOfLife.liveNeighboursAround(row = 1, column = 1)
+        aliveNeighbours `should be` 2
+
+        aliveNeighbours = gameOfLife.liveNeighboursAround(row = 1, column = 0)
+        aliveNeighbours `should be` 1
+
+        aliveNeighbours = gameOfLife.liveNeighboursAround(row = 0, column = 0)
+        aliveNeighbours `should be` 1
+
+        aliveNeighbours = gameOfLife.liveNeighboursAround(row = 0, column = 1)
+        aliveNeighbours `should be` 2
+    }
+
+    @Test
+    fun `count alive neighbours in a 3x3 grid`() {
+        val grid = arrayOf(
+            arrayOf('x', '.', 'x'),
+            arrayOf('.', '.', 'x'),
+            arrayOf('x', '.', '.'))
+        val gameOfLife = GameOfLife(grid)
+
+        val aliveNeighbours = gameOfLife.liveNeighboursAround(row = 1, column = 1)
+        aliveNeighbours `should be` 4
+    }
+
+    @Test
+    fun `any live cell with fewer than two live neighbours dies, as if caused by underpopulation`() {
+        val currentGrid = arrayOf(arrayOf('x', 'x', 'x'))
+        val nextGenGrid = arrayOf(arrayOf('.', 'x', '.'))
+        val gameOfLife = GameOfLife(currentGrid)
+
+        gameOfLife.nextGeneration() contentDeepEquals nextGenGrid `should be` true
+    }
+
+    @Test
+    fun `any live cell with more than three live neighbours dies, as if by overcrowding`() {
+        val currentGrid = arrayOf(
+            arrayOf('x', 'x', 'x'),
+            arrayOf('x', 'x', '.'))
+
+        val nextGenGrid = arrayOf(
+            arrayOf('x', '.', 'x'),
+            arrayOf('x', '.', 'x'))
+
+        val gameOfLife = GameOfLife(currentGrid)
+
+        gameOfLife.nextGeneration() contentDeepEquals nextGenGrid `should be` true
+    }
+
+    @Test
+    fun `any live cell with two or three live neighbours lives on to the next generation`() {
+        val currentGrid = arrayOf(
+            arrayOf('x', '.', '.'),
+            arrayOf('.', 'x', 'x'),
+            arrayOf('x', 'x', '.'))
+
+        val nextGenGrid = arrayOf(
+            arrayOf('.', 'x', '.'),
+            arrayOf('.', '.', 'x'),
+            arrayOf('x', 'x', 'x'))
+
+        val gameOfLife = GameOfLife(currentGrid)
+
+        gameOfLife.nextGeneration() contentDeepEquals nextGenGrid `should be` true
     }
 }
